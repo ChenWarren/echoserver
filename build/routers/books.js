3a56ab46e6a3ecac3b0340edece049ed92be3135
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-03-07 11:25:29 -0800

    delete build folder

--- build/routers/books.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const { getAllBooks, getBook } = require('../controllers/booksController');
-const router = (0, express_1.Router)();
-router.get('/:p', getAllBooks);
-router.get('/:id', getBook);
-module.exports = router;
