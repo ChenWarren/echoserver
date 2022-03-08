abeab0dc37dbf0fe2079ba455e10cfd2e2508a64
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2022-03-08 15:58:38 -0800

    update whitelist

--- build/config/whiteList.js
+++ build/config/whiteList.js
@@ -1,7 +1,7 @@
 "use strict";
 const whiteList = [
-    'https://www.web.com',
-    'http://127.0.0.1:5500',
+    'http://echoserver-env.eba-bccqr6dt.us-east-1.elasticbeanstalk.com',
+    'http://127.0.0.1:8080',
     'http://localhost:3500'
 ];
 module.exports = whiteList;
--- src/config/whiteList.ts
+++ src/config/whiteList.ts
@@ -1,7 +1,7 @@
 
 const whiteList: Array<string> = [
-    'https://www.web.com',
-    'http://127.0.0.1:5500',
+    'http://echoserver-env.eba-bccqr6dt.us-east-1.elasticbeanstalk.com',
+    'http://127.0.0.1:8080',
     'http://localhost:3500'
 ]
 
